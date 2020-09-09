07e115d4297b516595410c4a1596d2a4569a69fd
FRAZ5094 <60670235+FRAZ5094@users.noreply.github.com>
2020-09-09 22:38:29 +0100

    coding train 2.4

--- Nodejs/DataSelfie_Codingtrain/index.js
+++ Nodejs/DataSelfie_Codingtrain/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const Datastore=require("nedb");
 const app = express();
-app.listen(8080, () => console.log("listening at 3000"));
+app.listen(8080, () => console.log("listening at 8080"));
 app.use(express.static("public"));
 app.use(express.json({limit: "1mb"}));
+
+const database= new Datastore("database.db");
+database.loadDatabase();
 
 app.post("/api", (request, response) => {
     console.log("I got a request");
     console.log(request.body);
-    const data = request.body
-    response.json({status: "success", latitude: data.lat, longitude: data.lon});
+    const data = request.body;
+    const timestamp=Date.now();
+    data.timestamp=timestamp;
+    database.insert(data);
+    
+    response.json({
+        status: "success",
+        timestamp:timestamp,
+        latitude: data.lat,
+        longitude: data.lon
+    });
 });
